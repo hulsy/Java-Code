a8cd450e1a0ba7c17aea4e6a2ac6efd636634140
Alex Hulford <hulsy89@gmail.com>
2022-11-10 15:13:57 -0500

    added method to determine the sum of an array of integers without the smallest value in the array.

--- ArrayPracticeExcercises/src/ArrayPracticeExcercises.java
+++ ArrayPracticeExcercises/src/ArrayPracticeExcercises.java
@@ -5,7 +5,7 @@ public class ArrayPracticeExcercises {
 		
 	//write a for loop that prints 10 numbers and another that prints them in reverse
 		
-//		int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 //		
 //		for (int i=0; i<nums.length; i++) {
 //			System.out.println(nums[i]);
@@ -22,33 +22,55 @@ public class ArrayPracticeExcercises {
 	 * Only the first and last element.
 	 */
 	
-		int[] randomNums = new int[10];
+//		int[] randomNums = new int[10];
+//		
+//		for (int i=0; i<randomNums.length; i++) {
+//			randomNums[i] = (int) (Math.random() * 10);
+//			System.out.print(randomNums[i] + " ");
+//		}
+//		System.out.println();
+//		
+//		for (int i=0; i<randomNums.length; i++) {
+//			if (i % 2 == 0) {
+//				System.out.print(randomNums[i] + " ");
+//			}
+//		}
+//		System.out.println();
+//		
+//		for (int i=0; i<randomNums.length; i++) {
+//			if (randomNums[i] % 2 == 0) {
+//				System.out.print(randomNums[i] + " ");
+//			}
+//		}
+//		System.out.println();
+//		
+//		for (int i=randomNums.length - 1; i>=0; i--) {
+//			System.out.print(randomNums[i] + " ");
+//		}
+//		System.out.println();
+//		System.out.print(randomNums[0] + " " + randomNums[9]);
 		
-		for (int i=0; i<randomNums.length; i++) {
-			randomNums[i] = (int) (Math.random() * 10);
-			System.out.print(randomNums[i] + " ");
-		}
-		System.out.println();
+		/*
+		 * Write a method sumWithoutSmallest that computes the sum of an array of values, 
+		 * except for the smallest one, in a single loop.
+		 * In the loop, update the sum and the smallest value. 
+		 * After the loop, return the difference.
+		 */
+			
+		System.out.println(sumWithoutSmallest(nums));
 		
-		for (int i=0; i<randomNums.length; i++) {
-			if (i % 2 == 0) {
-				System.out.print(randomNums[i] + " ");
+	}
+	public static int sumWithoutSmallest(int[] values) {
+		int sumValues = 0;
+		int minValue = values[0];
+		for (int i=0; i<values.length; i++) {
+			if (values[i] < minValue) {
+				minValue = values[i];
 			}
+			sumValues += values[i];
 		}
-		System.out.println();
-		
-		for (int i=0; i<randomNums.length; i++) {
-			if (randomNums[i] % 2 == 0) {
-				System.out.print(randomNums[i] + " ");
-			}
-		}
-		System.out.println();
-		
-		for (int i=randomNums.length - 1; i>=0; i--) {
-			System.out.print(randomNums[i] + " ");
-		}
-		System.out.println();
-		System.out.print(randomNums[0] + " " + randomNums[9]);
+		sumValues -= minValue;
+		return sumValues;
 	}
 
 }
