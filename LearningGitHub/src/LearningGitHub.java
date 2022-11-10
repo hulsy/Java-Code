f39787df2971dc9b4130742610dd1e8b0d875099
Alex Hulford <hulsy89@gmail.com>
2022-11-10 11:43:47 -0500

    My first commit!!

---
+++ LearningGitHub/src/LearningGitHub.java
@@ -0,0 +1,12 @@
+
+public class LearningGitHub {
+
+	public static void main(String[] args) {
+		System.out.println("My first git commit");
+		
+		
+		System.out.println("my second commit");
+
+	}
+
+}
