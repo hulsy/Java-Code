70a1917517bc761531725a30915f04149ff6d21d
Alex Hulford <hulf0002@algonquinlive.com>
2022-12-01 12:59:12 -0500

    solved day1 challenge 2

--- AdventOfCode2022/src/Day1.java
+++ AdventOfCode2022/src/Day1.java
@@ -1,11 +1,14 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Day1 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader reader = new BufferedReader(new FileReader("input.txt"));
+		ArrayList<Integer> calorieCounter = new ArrayList<Integer>();
 		String line;
 		int calories = 0;
 		int mostCalories = 0;
@@ -15,12 +18,18 @@ public class Day1 {
 					if(mostCalories < calories) {
 						mostCalories = calories;
 					}
+					calorieCounter.add(calories);
 					calories = 0;
 					continue;
 				}
 					calories += Integer.parseInt(cal);	
 		}
-		System.out.println(mostCalories);
+		Collections.sort(calorieCounter, Collections.reverseOrder());
+		for(int i: calorieCounter) {
+			System.out.println(i);
+		}
+		int totalCalInventory = calorieCounter.get(0) + calorieCounter.get(1) + calorieCounter.get(2);
+		System.out.println(totalCalInventory);
 		reader.close();
 	}
 }
